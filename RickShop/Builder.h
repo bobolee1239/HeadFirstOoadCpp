4b2b355e0df5a2a3dead364015bcac060706fb2e
Brian Lee <bobolee1239@gmail.com>
2022-10-15 23:16:35 +0800

    refine code

--- RickShop/Builder.h
+++ RickShop/Builder.h
@@ -18,29 +18,14 @@ enum Builder {
 std::ostream& operator << (std::ostream& os, const Builder& builder) 
 {
     switch(builder) {
-      case FENDER:   
-        os << "Fender";
-        break;
-      case MARTIN:   
-        os << "Martin";
-        break;
-      case GIBSON:   
-        os << "Gibson";
-        break;
-      case COLLINGS: 
-        os << "Collings";
-        break;
-      case OLSON:    
-        os << "Olson";
-        break;
-      case RYAN:     
-        os << "Ryan";
-        break;
-      case PRS :     
-        os << "PRS";
-        break;
-      default:       
-        os << "Unspecified";
+      case FENDER:   os << "Fender"; break;
+      case MARTIN:   os << "Martin"; break;
+      case GIBSON:   os << "Gibson"; break;
+      case COLLINGS: os << "Collings"; break;
+      case OLSON:    os << "Olson"; break;
+      case RYAN:     os << "Ryan"; break;
+      case PRS :     os << "PRS"; break;
+      default:       os << "Unspecified"; break;
     }
     return os;
 }
